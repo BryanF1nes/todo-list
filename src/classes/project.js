d17a9bc38b45919cef6e94d105ef53a926ad378d
Bryan <bryanfinesw@gmail.com>
2025-11-22 23:41:14 -0500

    Added ESlint and Prettier

--- src/classes/project.js
+++ src/classes/project.js
@@ -1,31 +1,31 @@
 export class Project {
-    constructor(title) {
-        this.id = crypto.randomUUID();
-        this.title = title;
-        this.todos = [];
+  constructor(title) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.todos = [];
+  }
+
+  findTodo = (id) => {
+    const todo = this.todos.find((todo) => todo.id === id);
+    return todo;
+  };
+
+  addTodo = (todo) => {
+    this.todos.push(todo);
+    return;
+  };
+
+  removeTodo = (id) => {
+    const todo = this.findTodo(id);
+    this.todos.splice(todo, 1);
+    return;
+  };
+
+  editTodo = (id, values) => {
+    const todo = this.findTodo(id);
+    for (let [key, value] of Object.entries(values)) {
+      todo[key] = value;
     }
-
-    findTodo = (id) => {
-        const todo = this.todos.find((todo) => todo.id === id);
-        return todo;
-    };
-
-    addTodo = (todo) => {
-        this.todos.push(todo);
-        return this.todos.length;
-    };
-
-    removeTodo = (id) => {
-        const todo = this.findTodo(id);
-        this.todos.splice(todo, 1);
-        return this.todos.length;
-    };
-
-    editTodo = (id, values) => {
-        const todo = this.findTodo(id);
-        for (let [key, value] of Object.entries(values)) {
-            todo[key] = value;
-        };
-        return todo;
-    };
+    return todo;
+  };
 }
--- src/classes/todo.js
+++ src/classes/todo.js
@@ -1,10 +1,10 @@
 export class Todo {
-    constructor(title, description, dueDate, priority, project = 'Default') {
-        this.id = crypto.randomUUID();
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.project = project;
-    };
+  constructor(title, description, dueDate, priority, project = "Default") {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+  }
 }
